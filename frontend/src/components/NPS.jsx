8085e1a822f5bf8ba947fb6a9e6b24be4144451c
FreshMozzarella <alectaylor924@gmail.com>
2023-10-02 10:04:01 -0400

    fixed start time/end in NPS component

--- frontend/src/components/NPS.jsx
+++ frontend/src/components/NPS.jsx
@@ -40,112 +40,124 @@ export default function NPS() {
 
     fetchData();
   }, []);
-  
-    return (
-      <Box>
-        <Typography textAlign='center' variant="h3" component="h1">National Park Information</Typography>
-        <Divider variant='middle'/>
-        <Tabs value={tabValue} onChange={handleTabChange} sx={{    '& .MuiTabs-flexContainer': {
-      display: 'flex',
-      justifyContent: 'space-around',
-    }}} >
-          <Tab label="Alerts" value="alerts" variant="outlined" sx={{ flex: 1 }}/>
-          <Tab label="Events" value="events" sx={{ flex: 1 }}/>
-          <Tab label="Things To Do" value="thingstodo" sx={{ flex: 1 }}/>
-        </Tabs>
-  
-        {tabValue === 'alerts' && (
-          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
-            {data.alerts.map((alert) => (
-              <div key={alert.title} sx={{ padding: '0px', margin: '0px', flex: '1 0 auto' }}>
-                <Card sx={{ cursor: 'pointer', borderRadius: '8px', height: '70px' }} onClick={() => handleOpenModal(alert)}>
-                  <CardContent>
-                    <Typography variant="h6" component="h3" style={{ fontSize: '14px', color: 'grey' }}>
-                      {alert.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </div>
-            ))}
-          </div>
-        )}
-  
-        {tabValue === 'events' && (
-          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
-            {data.events.map((event) => (
-              <div key={event.title} sx={{ padding: '0px', margin: '0px', flex: '1 0 auto' }}>
-                <Card sx={{ cursor: 'pointer', borderRadius: '8px', height: '70px' }} onClick={() => handleOpenModal(event)}>
-                  <CardContent>
-                    <Typography variant="h6" component="h3" style={{ fontSize: '14px', color: 'grey' }}>
-                      {event.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </div>
-            ))}
-          </div>
-        )}
-  
-        {tabValue === 'thingstodo' && (
-          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
-            {data.thingstodo.map((thing) => (
-              <div key={thing.title} sx={{ padding: '0px', margin: '0px', flex: '1 0 auto' }}>
-                <Card sx={{ cursor: 'pointer', borderRadius: '8px', height: '70px' }} onClick={() => handleOpenModal(thing)}>
-                  <CardContent>
-                    <Typography variant="h6" component="h3" style={{ fontSize: '14px', color: 'grey' }}>
-                      {thing.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </div>
-            ))}
-          </div>
-        )}
-  
-        <Modal
-          open={openModal}
-          onClose={handleCloseModal}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Card>
-            <CardContent>
-              <Typography variant="body1" dangerouslySetInnerHTML={{ __html: selectedItem?.description }} />
-  
-              {tabValue === 'alerts' && (
-                <>
-                  {selectedItem?.url && <Typography variant="body2"><a href={selectedItem.url} target="_blank" rel="noopener noreferrer">link</a></Typography>}
 
-                </>
-              )}
-  
-              {tabValue === 'events' && (
-                <>
-                  {selectedItem?.datestart && <Typography variant="body2">From: {selectedItem?.datestart} to {selectedItem?.dateend}</Typography>}
-                  {selectedItem?.times && <Typography variant="body2">Hours: {Object.values(selectedItem?.times).join(', ')}</Typography>}
-                  {selectedItem?.isfree && <Typography variant="body2">Is it free?: {selectedItem?.isfree}</Typography>}
-                  {selectedItem?.feeinfo && <Typography variant="body2">Fee info: {selectedItem?.feeinfo}</Typography>}
-                </>
-              )}
-  
-              {tabValue === 'thingstodo' && (
-                <>
-                  {selectedItem?.duration && <Typography variant="body2">Average duration of activity: {selectedItem?.duration}</Typography>}
-                  {selectedItem?.activities && <Typography variant="body2">Activities: {selectedItem?.activities.join(', ')}</Typography>}
-                  {selectedItem?.feeDescription && <Typography variant="body2">Fee: {selectedItem?.feeDescription}</Typography>}
-                  {selectedItem?.season && <Typography variant="body2">Seasons available: {selectedItem?.season.join(', ')}</Typography>}
-                  {selectedItem?.arePetsPermitted && <Typography variant="body2">Are pets allowed?: {selectedItem?.arePetsPermitted}</Typography>}
-                </>
-              )}
-  
-              {selectedItem?.longitude && <Typography variant="body2">Exact location: {selectedItem?.longitude} - {selectedItem?.latitude}</Typography>}
-            </CardContent>
-          </Card>
-        </Modal>
-      </Box>
-    );
-  }
-  
+  return (
+    <Box>
+      <Typography textAlign='center' variant="h3" component="h1">National Park Information</Typography>
+      <Divider variant='middle' />
+      <Tabs value={tabValue} onChange={handleTabChange} sx={{
+        '& .MuiTabs-flexContainer': {
+          display: 'flex',
+          justifyContent: 'space-around',
+        }
+      }} >
+        <Tab label="Alerts" value="alerts" variant="outlined" sx={{ flex: 1 }} />
+        <Tab label="Events" value="events" sx={{ flex: 1 }} />
+        <Tab label="Things To Do" value="thingstodo" sx={{ flex: 1 }} />
+      </Tabs>
+
+      {tabValue === 'alerts' && (
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+          {data.alerts.map((alert) => (
+            <div key={alert.title} sx={{ padding: '0px', margin: '0px', flex: '1 0 auto' }}>
+              <Card sx={{ cursor: 'pointer', borderRadius: '8px', height: '70px' }} onClick={() => handleOpenModal(alert)}>
+                <CardContent>
+                  <Typography variant="h6" component="h3" style={{ fontSize: '14px', color: 'grey' }}>
+                    {alert.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {tabValue === 'events' && (
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+          {data.events.map((event) => (
+            <div key={event.title} sx={{ padding: '0px', margin: '0px', flex: '1 0 auto' }}>
+              <Card sx={{ cursor: 'pointer', borderRadius: '8px', height: '70px' }} onClick={() => handleOpenModal(event)}>
+                <CardContent>
+                  <Typography variant="h6" component="h3" style={{ fontSize: '14px', color: 'grey' }}>
+                    {event.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {tabValue === 'thingstodo' && (
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+          {data.thingstodo.map((thing) => (
+            <div key={thing.title} sx={{ padding: '0px', margin: '0px', flex: '1 0 auto' }}>
+              <Card sx={{ cursor: 'pointer', borderRadius: '8px', height: '70px' }} onClick={() => handleOpenModal(thing)}>
+                <CardContent>
+                  <Typography variant="h6" component="h3" style={{ fontSize: '14px', color: 'grey' }}>
+                    {thing.title}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Card>
+          <CardContent>
+            <Typography variant="body1" dangerouslySetInnerHTML={{ __html: selectedItem?.description }} />
+
+            {tabValue === 'alerts' && (
+              <>
+                {selectedItem?.url && <Typography variant="body2"><a href={selectedItem.url} target="_blank" rel="noopener noreferrer">link</a></Typography>}
+
+              </>
+            )}
+
+            {tabValue === 'events' && (
+              <>
+                {selectedItem?.datestart && <Typography variant="body2">From: {selectedItem?.datestart} to {selectedItem?.dateend}</Typography>}
+                {selectedItem?.times && (
+                  <Typography variant="body2">
+                    Hours:
+                    {selectedItem.times.map((timeObj, index) => (
+                      <span key={index}>
+                        {timeObj.timestart} - {timeObj.timeend}
+                        {index < selectedItem.times.length - 1 && ', '}
+                      </span>
+                    ))}
+                  </Typography>
+                )}
+                {selectedItem?.isfree && <Typography variant="body2">Is it free?: {selectedItem?.isfree}</Typography>}
+                {selectedItem?.feeinfo && <Typography variant="body2">Fee info: {selectedItem?.feeinfo}</Typography>}
+              </>
+            )}
+
+            {tabValue === 'thingstodo' && (
+              <>
+                {selectedItem?.duration && <Typography variant="body2">Average duration of activity: {selectedItem?.duration}</Typography>}
+                {selectedItem?.activities && <Typography variant="body2">Activities: {selectedItem?.activities.join(', ')}</Typography>}
+                {selectedItem?.feeDescription && <Typography variant="body2">Fee: {selectedItem?.feeDescription}</Typography>}
+                {selectedItem?.season && <Typography variant="body2">Seasons available: {selectedItem?.season.join(', ')}</Typography>}
+                {selectedItem?.arePetsPermitted && <Typography variant="body2">Are pets allowed?: {selectedItem?.arePetsPermitted}</Typography>}
+              </>
+            )}
+
+            {selectedItem?.longitude && <Typography variant="body2">Exact location: {selectedItem?.longitude} - {selectedItem?.latitude}</Typography>}
+          </CardContent>
+        </Card>
+      </Modal>
+    </Box>
+  );
+}
+
 
 
 
